bb12bdf158a1f2beb6246a7deeb5ba26e5af62e6
Aeralyn <Aeralyn123@gmail.com>
2025-02-20 19:33:51 -0700

    Adding footer to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <main className='container pt-5'>
+      <main className="container pt-5">
         <Outlet />
       </main>
+      <footer className="text-center py-3">
+        <p>Neff, Cabrera, Angulo, Moriasi ©</p>
+      </footer>
     </div>
   );
 }
