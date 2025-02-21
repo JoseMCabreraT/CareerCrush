75d396d194d98b9b32ee47382cf47d0550208fa8
Jose Cabrera <josemcabrerat@gmail.com>
2025-02-20 20:35:33 -0700

    The Job list is being displayed on the page after Log In.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,23 @@
 import { useState, useEffect, useLayoutEffect } from "react";
-import { retrieveUsers } from "../api/userAPI";
-import type { UserData } from "../interfaces/UserData";
+//import { retrieveUsers } from "../api/userAPI";
+//import type { UserData } from "../interfaces/UserData";
 import ErrorPage from "./ErrorPage";
-import UserList from '../components/Users';
+//import UserList from '../components/Users';
 import auth from '../utils/auth';
 import JobListPage from '../pages/JobListPage';
+import { JobAttributes } from "../../../server/src/models/jobs";
+import { retrieveJobs } from "../api/jobAPI";
 
 const Home = () => {
 
-    const [users, setUsers] = useState<UserData[]>([]);
+    //const [users, setUsers] = useState<UserData[]>([]);
     const [error, setError] = useState(false);
     const [loginCheck, setLoginCheck] = useState(false);
+    const [jobs, setJobs] = useState<JobAttributes[] | null>(null);
 
     useEffect(() => {
         if (loginCheck) {
-            fetchUsers();
+            fetchJobs();
         }
     }, [loginCheck]);
 
@@ -28,7 +31,7 @@ const Home = () => {
         }
     };
 
-    const fetchUsers = async () => {
+    /*const fetchUsers = async () => {
         try {
             const data = await retrieveUsers();
             setUsers(data)
@@ -36,7 +39,20 @@ const Home = () => {
             console.error('Failed to retrieve tickets:', err);
             setError(true);
         }
-    }
+    }*/
+
+    const fetchJobs = async () => {
+        try {
+            const data = await retrieveJobs();
+            setJobs(data)
+        } catch (err) {
+            console.error('Failed to retrieve jobs', err);
+            setError(true);
+        }
+
+    }//fetchJobs
+
+
 
     if (error) {
         return <ErrorPage />;
@@ -52,7 +68,7 @@ const Home = () => {
                         </h1>
                     </div>
                 ) : (
-                    <UserList users={users} />
+                    <JobListPage jobs={jobs} />
                 )}
         </>
     );
