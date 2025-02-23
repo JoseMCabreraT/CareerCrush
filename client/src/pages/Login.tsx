257ab567392ced5bc05a646df16eed9d261e4527
a-angulo <alexabdoulah@gmail.com>
2025-02-23 13:03:53 -0800

    updates02.23.25 part 2

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,21 @@
-import { useState, type FormEvent, type ChangeEvent } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import Auth from '../utils/auth';
-import { login } from '../api/authAPI';
-import type { UserLogin } from '../interfaces/UserLogin';
+import { useState, type FormEvent, type ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import Auth from "../utils/auth";
+import { login } from "../api/authAPI";
+import type { UserLogin } from "../interfaces/UserLogin";
 
 const Login = () => {
   const [loginData, setLoginData] = useState<UserLogin>({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const navigate = useNavigate(); 
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setLoginData({
       ...loginData,
@@ -24,49 +26,49 @@ const Login = () => {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
     try {
       const data = await login(loginData);
       Auth.login(data.token);
-      navigate('/');  
+      navigate("/");
     } catch (err) {
-      setError('Failed to login. Please check your credentials and try again.');
-      console.error('Failed to login', err);
+      setError("Failed to login. Please check your credentials and try again.");
+      console.error("Failed to login", err);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className='form-container'>
-      <form className='form login-form' onSubmit={handleSubmit}>
+    <div className="form-container">
+      <form className="form login-form" onSubmit={handleSubmit}>
         <h1>Login</h1>
-        {error && <p className='error'>{error}</p>}
-        <div className='form-group'>
-          <label htmlFor='username'>Username</label>
+        {error && <p className="error">{error}</p>}
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
           <input
-            className='form-input'
-            type='text'
-            name='username'
-            id='username'
+            className="form-input"
+            type="text"
+            name="username"
+            id="username"
             value={loginData.username}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
           <input
-            className='form-input'
-            type='password'
-            name='password'
-            id='password'
+            className="form-input"
+            type="password"
+            name="password"
+            id="password"
             value={loginData.password}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group'>
-          <button className='btn btn-primary' type='submit' disabled={loading}>
-            {loading ? 'Logging In...' : 'Login'}
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit" disabled={loading}>
+            {loading ? "Logging In..." : "Login"}
           </button>
         </div>
       </form>
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,83 +1,85 @@
-import { useState, type FormEvent, type ChangeEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, type FormEvent, type ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
 //import Auth from '../utils/auth';
-//import { login } from '../api/authAPI';
-//import type { UserLogin } from '../interfaces/UserLogin';
+import { signup } from "../api/authAPI";
+import type { Usersignup } from "../interfaces/UserLogin";
 
 const Signup = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [signupData, setSignupData] = useState<Usersignup>({
-    username: '',
-    email: '',
-    password: '',
+    username: "",
+    email: "",
+    password: "",
   });
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setSignupData({
       ...signupData,
-      [name]: value, 
+      [name]: value,
     });
   };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
     try {
       await signup(signupData);
-      navigate('/'); 
+      navigate("/");
     } catch (err) {
-      setError('Failed to Signup. Please try again.');
-      console.error('Failed to Signup', err);
+      setError("Failed to Signup. Please try again.");
+      console.error("Failed to Signup", err);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className='form-container'>
-      <form className='form login-form' onSubmit={handleSubmit}>
+    <div className="form-container">
+      <form className="form login-form" onSubmit={handleSubmit}>
         <h1>Signup</h1>
-        {error && <p className='error'>{error}</p>}
-        <div className='form-group'>
-          <label htmlFor='username'>Name</label>
+        {error && <p className="error">{error}</p>}
+        <div className="form-group">
+          <label htmlFor="username">Name</label>
           <input
-            className='form-input'
-            type='text'
-            name='username'
-            id='username'
+            className="form-input"
+            type="text"
+            name="username"
+            id="username"
             value={signupData.username}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email</label>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
           <input
-            className='form-input'
-            type='email'
-            name='email'
-            id='email'
+            className="form-input"
+            type="email"
+            name="email"
+            id="email"
             value={signupData.email}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
           <input
-            className='form-input'
-            type='password'
-            name='password'
-            id='password'
+            className="form-input"
+            type="password"
+            name="password"
+            id="password"
             value={signupData.password}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group'>
-          <button className='btn btn-primary' type='submit' disabled={loading}>
-            {loading ? 'Signing Up...' : 'Signup'}
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit" disabled={loading}>
+            {loading ? "Signing Up..." : "Signup"}
           </button>
         </div>
       </form>
