ae1f426b6b2dce18d0db165067dca376c2842605
stanmoriasi <stanmoriasi@yahoo.com>
2025-02-24 13:41:57 -0700

    apply consistent formatting to seed file

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,17 +3,22 @@ import { User } from '../models/index.js';
 export const seedUsers = async () => {
   await User.bulkCreate(
     [
-      { username: 'JollyGuru', email: 'jolly@guru.com', password: 'password' },
+      { username: 'JollyGuru', 
+        email: 'jolly@guru.com', 
+        password: 'password' 
+      },// 1 
       {
         username: 'SunnyScribe',
         email: 'sunny@scribe.com',
         password: 'password',
-      },
+      },// 2
       {
         username: 'RadiantComet',
         email: 'radiant@comet.com',
-        password: 'password',
-      },
+        password: 'password' 
+      },// 3
+
+      
     ],
     { individualHooks: true }
   );
